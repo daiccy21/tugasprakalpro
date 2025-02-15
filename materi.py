0219602009f5cb7d07702d286b8ecfb2ffd123b9
daiccy21 <dannielsantosa@gmail.com>
2025-02-15 16:32:05 +0700

    Add files via upload

---
+++ materi.py
@@ -0,0 +1,12 @@
+x1 = eval(input("X1 = "))
+x2 = eval(input("X2 = "))
+x3 = eval(input("X3 = "))
+x4 = eval(input("X4 = "))
+jumlah = x1+x2+x3+x4
+kali = x1*x2*x3*x4
+print('Hasil Penjumlah semua bilangan = ', jumlah)
+print('Hasil Perkalian semua bilangan = ', kali)
+jumlah = jumlah + 0.5
+print('Jika ditambah 0.5 hasilnya = ',jumlah)
+kali = kali * 0.5
+print('Jika dikali 0.5 hasilnya = ',kali)
